a098619e37babedcb9192f8ce9a525321cc12cc6
Priscakoech <priscakoech448@gmail.com>
2025-07-06 23:12:09 +0300

    Added the yield generator

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -15,7 +15,9 @@ def stream_users_in_batches(batch_size):
     connection.close()
 
 def batch_processing(batch_size):
+    filtered_users = []
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user['age'] > 25:
-                print(user)
+                filtered_users.append(user)
+                return filtered_users
