dea2eb21e9e3726c4194ee0cbae4d1ccf1556d0e
Priscakoech <priscakoech448@gmail.com>
2025-07-20 23:03:01 +0300

    Add

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status, filters
 from rest_framework.permissions import IsAuthenticated
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
