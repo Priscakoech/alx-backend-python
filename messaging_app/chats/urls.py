caa77ba48184bf262b6a27eee89b23a40259338c
Priscakoech <priscakoech448@gmail.com>
2025-07-20 23:14:38 +0300

    added nested routers

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -6,6 +6,10 @@ router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
+convo_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+convo_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+
+
 urlpatterns = [
     path('', include(router.urls))
 ]
