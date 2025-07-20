dc7e42df8f7c94e5f677cede4fe92313fe980776
Priscakoech <priscakoech448@gmail.com>
2025-07-20 18:03:37 +0300

    Added a ccontext manager to test that a keyerror is raised

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -12,6 +12,15 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
         
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b"),     
+        ])
+    def test_access_nested_map_raises(self, nested_map, path, expected):
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), expected)
+        
         
 if __name__ == "__main__":
     unittest.main()
